fb3865d421bf0002b044a90559e53566d170ae44
Franklin Tarter <franklintarter@gmail.com>
2019-10-06 15:22:37 -0700

    cleanup example

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -5,13 +5,5 @@
  */
 
 module.exports = {
-  plugins: [
-    `gatsby-theme-blog-ml-core`,
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-  ],
+  plugins: [`gatsby-theme-blog-ml-core`, "gatsby-plugin-netlify-cms"],
 }
--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 import { Layout } from 'gatsby-theme-blog-ml-core/src/components'
-export default () => (<Layout><div>Hello world!</div></Layout>)
+export default () => (
+  <Layout>
+    <div>
+      <h1>Home</h1>
+    </div>
+  </Layout>
+)
